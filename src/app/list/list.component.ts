65f657fe99677cdf6ed0dd90c7f5b3b6797a03ba
quanghuy24121999 <quanghuy24121999@gmail.com>
2020-10-21 08:52:03 +0700

    binding data

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  products = [
+    {
+      name: 'Iphone X',
+      description: 'This is Iphone X',
+      image: 'https://via.placeholder.com/200x150',
+      price: 5.99,
+      quantity: 2
+    },
+    {
+      name: 'Iphone XS Max',
+      description: 'This is Iphone XS Max',
+      image: 'https://via.placeholder.com/200x150',
+      price: 9.99,
+      quantity: 2
+    }
+  ];
 
   constructor() { }
 
