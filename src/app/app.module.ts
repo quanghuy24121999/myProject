45a99690d7b8ce29a7e0553e5f28150e82839cdd
quanghuy24121999 <quanghuy24121999@gmail.com>
2020-10-21 08:36:14 +0700

    update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ListComponent } from './list/list.component';
+import { PromoCodeComponent } from './promo-code/promo-code.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, SummaryComponent, ListComponent, PromoCodeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
